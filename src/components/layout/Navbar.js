63936f31c811d2a4b3d13f3dc8d3769b3a87c78e
ponntp <nathapon1161@gmail.com>
2021-10-16 19:54:12 +0700

    Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,13 +3,26 @@ import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
+import SignInpa from './SignInpa'
 
 
 const Navbar = (props) => {
   const { auth, profile } = props;
   // console.log(auth);
-  const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
+  let links
+  if (auth.email != null && auth.phoneNumber == null) 
+  {
+    links = auth.email ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
+  } 
+  else if (auth.phoneNumber != null && auth.email == null) 
+  {
+     links = auth.phoneNumber ? <SignInpa profile={profile} /> : <SignedOutLinks />;
+  }
+  else
+  { 
+    links = auth.email ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
 
+  }
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
--- src/components/test2.js
+++ src/components/test2.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './test2.css';
-import NavbarVo from './layout/NavbarVo'
+
 
 export default class test2 extends Component {
     render() {
         return ( 
             <div>
-                <NavbarVo />
+             
                 <h2>Volunteer Home</h2>
             </div>
         )
